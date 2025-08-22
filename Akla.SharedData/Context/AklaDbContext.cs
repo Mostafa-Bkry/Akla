ef00ec465e32c51b846b0ce31b6fdf6ad5ad4927
Mostafa Bkry <mbkry661@gmail.com>
2025-08-22 22:44:19 +0300

    Created Repo and webapi projects and registered the dbcontext service and made init migration

--- Akla.SharedData/Context/AklaDbContext.cs
+++ Akla.SharedData/Context/AklaDbContext.cs
@@ -4,6 +4,9 @@ namespace Akla.SharedData.Context
 {
     public class AklaDbContext : DbContext
     {
+
+        public AklaDbContext(DbContextOptions<AklaDbContext> options) : base(options) { }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
 
---
+++ Akla.WebAPI/Program.cs
@@ -0,0 +1,39 @@
+
+using Akla.SharedData.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Akla.WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDbContext<AklaDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("AklaConnectionString")
+                )
+            );
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
